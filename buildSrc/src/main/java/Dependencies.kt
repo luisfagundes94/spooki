2b08c891bd993d57c29cc5b49ef7c5bd34c70100
luisfagundes94 <luisfelipecf94@gmail.com>
2022-02-06 20:05:53 -0300

    adding buildSrc directory

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,60 @@
+package com.luisfelipe.buildSrc
+
+object Dependencies {
+
+    object Core {
+        const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+        const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+        const val material = "com.google.android.material:material:${Versions.material}"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+        const val coroutines_core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+        const val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+        const val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+        const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+    }
+
+    object Data {
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        const val gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+        const val okHttp = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+        const val preferencesKtx = "androidx.preference:preference-ktx:${Versions.preferencesKtx}"
+        const val roomRuntime = "androidx.room:room-runtime:${Versions.room}"
+        const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
+        const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+        const val loggingInterceptor = "com.github.ihsanbal:LoggingInterceptor:${Versions.loggingInterceptor}"
+    }
+
+    object UI {
+        const val picasso = "com.squareup.picasso:picasso:${Versions.picasso}"
+        const val cardView = "androidx.cardview:cardview:${Versions.card_view}"
+        const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+        const val lottieAnimation = "com.airbnb.android:lottie:${Versions.lottieAnimation}"
+        const val shimmerEffect = "com.facebook.shimmer:shimmer:${Versions.shimmer}"
+        const val tagGroup = "com.github.pchmn:MaterialChipsInput:${Versions.tagGroup}"
+    }
+
+    object DI {
+        const val koin = "io.insert-koin:koin-android:${Versions.koin}"
+        const val koinCore = "io.insert-koin:koin-core:${Versions.koin}"
+    }
+
+    object Gradle {
+        const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+        const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+        const val ktlintPlugin = "com.pinterest:ktlint:${Versions.ktlint}"
+        const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    object Test {
+        const val junit = "junit:junit:${Versions.junit}"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
+        const val mockk = "io.mockk:mockk:${Versions.mockk}"
+        const val archCore = "androidx.arch.core:core-testing:${Versions.archCore}"
+    }
+
+    object Lifecycle {
+        const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.liveData}"
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,52 @@
+package com.luisfelipe.buildSrc
+
+object Versions {
+
+    // Build.Gradle Config
+    const val appId = "com.luisfelipe.spooki"
+    const val compileSdk = 31
+    const val buildTools = "31.0.0"
+    const val jvmTargetVersion = "1.8"
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val ktlint = "0.43.2"
+    const val gradle = "7.0.4"
+
+    // Core
+    const val kotlin = "1.6.10"
+    const val appcompat = "1.4.1"
+    const val material = "1.5.0"
+    const val coreKtx = "1.7.0"
+    const val constraintLayout = "2.1.3"
+    const val navigation = "2.3.1"
+    const val timber = "5.0.1"
+
+    // Data
+    const val retrofit = "2.9.0"
+    const val okHttp = "4.2.1"
+    const val preferencesKtx = "1.1.0"
+    const val room = "2.2.6"
+    const val loggingInterceptor = "3.1.0"
+
+    // UI
+    const val picasso = "2.71828"
+    const val card_view = "1.0.0"
+    const val recyclerView = "1.2.1"
+    const val lottieAnimation = "4.2.2"
+    const val shimmer = "0.5.0"
+    const val tagGroup = "1.0.8"
+
+    // DI
+    const val koin = "3.1.4"
+
+    // Test
+    const val junit = "4.13.1"
+    const val coroutines = "1.3.6"
+    const val mockk = "1.12.1"
+    const val archCore = "2.1.0"
+
+    // Lifecycle
+    const val liveData = "2.2.0"
+}
