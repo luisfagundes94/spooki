04650b4c62fdb064993a6ab417aa0773db83f5da
luisfagundes94 <luisfelipecf94@gmail.com>
2022-02-12 12:42:40 -0300

    adding UI states to home fragment

--- features/home/src/main/java/com/luisfelipe/home/presentation/HomeFragment.kt
+++ features/home/src/main/java/com/luisfelipe/home/presentation/HomeFragment.kt
@@ -1,7 +1,17 @@
 package com.luisfelipe.home.presentation
 
-import androidx.fragment.app.Fragment
+import com.luisfelipe.base.BaseFragment
 import com.luisfelipe.home.R
+import com.luisfelipe.home.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(
+    successViewId = R.id.home_success,
+    loadingViewId = R.id.home_loading,
+    errorViewId = R.id.home_error
+) {
+    override fun onBind() = FragmentHomeBinding.inflate(layoutInflater)
+
+    override fun FragmentHomeBinding.onViewCreated() {
+        showError()
+    }
 }
