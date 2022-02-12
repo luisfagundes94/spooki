212ad22d57da3395e3afe8a6faa5d664c57afd8a
luisfagundes94 <luisfelipecf94@gmail.com>
2022-02-11 22:38:24 -0300

    adding main bottom nav fragments

---
+++ features/saved/src/main/java/com/luisfelipe/saved/presentation/SavedFragment.kt
@@ -0,0 +1,7 @@
+package com.luisfelipe.saved.presentation
+
+import androidx.fragment.app.Fragment
+import com.luisfelipe.saved.R
+
+class SavedFragment : Fragment(R.layout.fragment_saved) {
+}
---
+++ features/search/src/main/java/com/luisfelipe/search/presentation/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.luisfelipe.search.presentation
+
+import androidx.fragment.app.Fragment
+import com.luisfelipe.search.R
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+}
